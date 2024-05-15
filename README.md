c0349d135e435b1fdd8ae535f2792077c6261502
Nguyễn Ngọc Đức <ducnn@outlook.com>
2024-05-16 00:30:27 +1000

    text

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# value-type-reference-type
+# Giới thiệu
+Nội dung bài viết này đề cập đến kiểu giá trị và kiểu tham chiếu trong C# (và có thể một số ngôn ngữ khác như Java). C# không có kiểu con trỏ và không cần người dùng quản lý bộ nhớ. Nhưng về mặt gốc rễ, con trỏ và quản lý bộ nhớ vẫn tồn tại và chúng ta cần hiểu về cách thức mà nó hoạt động để hiểu rõ kiểu giá trị, kiểu tham thiếu, từ khóa ref và out.
+
+# Stack - Heap
+
+
--- ValueTypeReferenceType/ReferenceTypeExplaination.cs
+++ ValueTypeReferenceType/ReferenceTypeExplaination.cs
@@ -6,6 +6,12 @@ public class ReferenceTypeExplaination
     {
         Console.WriteLine("Giai thich ve reference type");
 
+        // vì là kiểu reference, khi truyền vào hàm, ta thấy có 2 điểm sau:
+        // giống với kiểu giá trị, giá trị của origin cũng được copy vào hàm
+        // nhưng giá trị này là địa chỉ của object instance trong Heap
+        // vì thế khi đổi giá trị property của object, khi ra ngoài hàm,
+        // thay đổi này được giữ nguyên vì nó được thực hiện trong Heap
+        // và origin trong Stack vẫn là địa chỉ của instance trong Heap
         var origin = new ReferenceClass { Name = "Name" };
 
         var setNameObj = SetName(origin);
@@ -16,6 +22,12 @@ public class ReferenceTypeExplaination
 
         Console.WriteLine($"setName: {setNameObj.Name}");
 
+        // Giá trị trả về của hàm là giá trị của origin trong Stack
+        // giá trị này được gán cho ô nhớ của setNameObj trong Stack
+        // origin và setNameObj là hai biến có hai vị trí trong Stack
+        // và có cùng giá trị là địa chỉ của instance trong Heap
+        // khi gán new instance cho setNameObj, giá trị của setNameObj trong Stack
+        // sẽ chuyển thành địa chỉ của new instance trong Heap
         Console.WriteLine("Khoi tao gia tri moi cua setNameObj");
 
         setNameObj = new ReferenceClass { Name = "New" };
@@ -24,6 +36,10 @@ public class ReferenceTypeExplaination
 
         Console.WriteLine($"setName: {setNameObj.Name}");
 
+        // Đoạn này để chứng minh giá trị của origin truyền vào hàm đã được copy
+        // obj là biến cục bộ mới, được nhận giá trị của origin mà thôi
+        // khi gán new instance cho obj, giá trị của obj chuyển thành địa chỉ
+        // của new instance trong Heap, không phải origin được cập nhật
         Console.WriteLine("Khoi tao gia tri moi obj trong ham SetName");
 
         var setNameNew = SetNameAndNew(origin);
@@ -41,6 +57,11 @@ public class ReferenceTypeExplaination
 
         var origin = new ReferenceClass { Name = "Name" };
 
+        // đoạn đầu tiên sẽ thay đổi Name với param truyền vào là ref origin
+        // đoạn này không khác đoạn không dùng ref, vì cùng sử dụng chung một giá trị trong Stack
+        // giá trị này trỏ đến instance trong Heap
+        // giá trị trả về của hàm là giá trị trong Stack, là địa chỉ của instance trong Heap
+        // giá trị này được gán vào ô nhớ của setNameObj trong Stack (không dùng ô nhớ của origin)
         var setNameObj = SetNameRef(ref origin);
 
         Console.WriteLine("Doi ten doi tuong origin");
@@ -49,7 +70,11 @@ public class ReferenceTypeExplaination
 
         Console.WriteLine($"setName: {setNameObj.Name}");
 
-        Console.WriteLine("Khoi tao gia tri moi obj trong ham SetName");
+        // đoạn này để chứng minh setNameObj và origin có 2 ô nhớ riêng có cùng giá trị
+        // là địa chỉ của instance trong Heap
+        // gán new instance cho setNameObj, giá trị của setNameObj trong Stack sẽ là
+        // địa chỉ của new instance trong Heap
+        Console.WriteLine("Khoi tao gia tri moi obj sau khi goi SetName");
 
         setNameObj = new ReferenceClass { Name = "New" };
 
@@ -57,6 +82,12 @@ public class ReferenceTypeExplaination
 
         Console.WriteLine($"setName: {setNameObj.Name}");
 
+        // Trong ví dụ này, hàm SetNameRefAndNew sẽ thay đổi Name của origin
+        // sau đó gán origin bằng đối tượng mới và trả về origin.
+        // Như vậy, giá trị của origin trong Stack bị thay đổi vì chúng ta truyền ref origin vào hàm
+        // giá trị trong Stack này trỏ đến vùng nhớ của instance của ReferenceClass trong Heap
+        // giá trị này thành giá trị trả về, gán cho setNameNew
+        // vậy setNameNew có một ô nhớ trong Stack có giá trị giống như origin và cùng trỏ tới new instance
         Console.WriteLine("Khoi tao gia tri moi obj trong ham SetName");
 
         var setNameNew = SetNameRefAndNew(ref origin);
@@ -70,8 +101,15 @@ public class ReferenceTypeExplaination
 
     public void ExplainOut()
     {
+        // về cơ bản, toàn bộ hàm này để chứng minh out an toàn hơn ref
+        // out đòi hỏi lập trình viên phải tạo biến để truyền vào
+        // vì có biến mới nên biến này có ô nhớ riêng trong Stack
+        // bảo vệ giá trị của origin trong Stack không bị xâm hại
         Console.WriteLine("Giai thich ve reference type voi out");
 
+        // biến out nhận giá trị của origin trong Stack, có ô nhớ riêng trong Stack
+        // setNameObj nhận giá trị của origin trong Stack, có ô nhớ riêng trong Stack
+        // ba biến này độc lập nhưng có cùng giá trị trong Stack là địa chỉ của instance trong Heap
         var origin = new ReferenceClass { Name = "Name" };
 
         ReferenceClass outObj;
@@ -86,6 +124,8 @@ public class ReferenceTypeExplaination
 
         Console.WriteLine($"outObj: {outObj.Name}");
 
+        // Vì cả 3 biến dù độc lập nhưng đều trỏ đến cùng 1 instance trong Heap
+        // đổi property là cùng thao tác trên 1 instance
         Console.WriteLine("Doi ten tung obj");
 
         origin.Name += "origin";
@@ -100,6 +140,9 @@ public class ReferenceTypeExplaination
 
         Console.WriteLine($"outObj: {outObj.Name}");
 
+        // Vì 3 biến độc lập, có 3 ô nhớ riêng trong Stack
+        // khi gán new instance cho out và return
+        // 3 biến này sẽ có giá trị trong Stack là 3 địa chỉ của 3 instance trong Heap
         Console.WriteLine("Khoi tao doi tuong moi cho out va return");
 
         outObj = new ReferenceClass { Name = "out" };
@@ -112,9 +155,12 @@ public class ReferenceTypeExplaination
 
         Console.WriteLine($"outObj: {outObj.Name}");
 
+        // đoạn này để cho thấy, 3 biến là độc lập
+        // khi gán giá trị thì chỉ giá trị trong Stack được copy sang
+        // kết quả là giống với các trường hợp trên
         Console.WriteLine("Dat bien out bang bien ref");
 
-        ReferenceClass out2 = new ReferenceClass { Name = "out" };
+        ReferenceClass out2;
 
         var return2 = SetNameRefOut(ref origin, out out2);
 
